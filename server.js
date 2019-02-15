373e6a2535c5383b32b334d813f9365b9c58e41e
FX-Wood <fxwood2@gmail.com>
2019-02-14 16:52:18 -0800

    committing working routes

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const app = express();
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html');
+    res.sendFile('views/index.html');
 })
 
 app.get('/about', function(req, res) {
-    res.sendFile('about.html');
+    res.sendFile(__dirname + '/views/about.html');
 })
 
 app.get('/skills', function(req, res) {
-    res.sendFile('skills.html');
+    res.sendFile(__dirname + '/views/skills.html');
 })
 
 app.get('/contact', function(req, res) {
-    res.sendFile('contact.html');
+    res.sendFile(__dirname + '/views/contact.html');
 })
 app.listen(3000, function() {
     console.log("You are listening to port 3000");
